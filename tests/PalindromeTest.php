9845c3bedeb85a3503b44b378a3e1a8501fd7544
Casey Heitz & Sam Martinez <student@epicodus.com>
2015-08-12 15:06:22 -0700

    Add third test for non-palindromes

--- tests/PalindromeTest.php
+++ tests/PalindromeTest.php
@@ -14,4 +14,24 @@
 
             $this->assertEquals(true, $result);
         }
+
+        function test_one_word_true()
+        {
+            $check_Palindrome = new Palindrome;
+            $input = "racecar";
+
+            $result = $check_Palindrome->test_Palindrome($input);
+
+            $this->assertEquals(true, $result);
+        }
+
+        function test_one_word_false()
+        {
+            $check_Palindrome = new Palindrome;
+            $input = "taco";
+
+            $result = $check_Palindrome->test_Palindrome($input);
+
+            $this->assertEquals(false, $result);
+        }
     }
