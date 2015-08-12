6c7953b7eb839fc57960945bebe86470b00ca327
Casey Heitz & Sam Martinez <student@epicodus.com>
2015-08-12 16:11:43 -0700

    add hard way

--- src/Palindrome.php
+++ src/Palindrome.php
@@ -4,7 +4,21 @@
     {
         function test_Palindrome($input)
         {
-            if($input == strrev($input)) {
+
+            //Below is the code to have this work the easy way
+        /*    if($input == strrev($input)) {
+                return true;
+            } */
+
+            //This is considered to be the hard way for this function to work without using strrev
+            $input_array = str_split($input);
+            $reversed_array = array();
+            
+            for($i = sizeof($input_array)-1; $i >= 0; $i--) {
+                array_push($reversed_array, $input_array[$i]);
+            }
+
+            if($input_array == $reversed_array) {
                 return true;
             }
         }
